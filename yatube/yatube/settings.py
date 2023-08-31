8423a1d0aed3e7dab091fd995164ba9730e33ef6
ArtemL <sntchweb@yandex.ru>
2023-08-31 15:45:37 +0400

    nnn

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,17 +1,12 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'wxetluluioqusk$ho2t!^50%36rj#v_&&z$&a=qp2rb-h4)jmf'
+
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -52,16 +47,27 @@ MIDDLEWARE = [
 ]
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
 LOGIN_URL = 'users:login'
+
 LOGIN_REDIRECT_URL = 'posts:index'
+
 # LOGOUT_REDIRECT_URL = 'posts:index'
+
 ROOT_URLCONF = 'yatube.urls'
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
